25cb88d7d10834408ed78a4c06cedcba5e0e14ac
Renan Bastos <renanbastos.tec@gmail.com>
2017-03-07 17:19:01 -0300

    Removed @ and / of the content social.

--- js/content.js
+++ js/content.js
@@ -2,22 +2,22 @@
 	'use strict';
 
 	const RenanContent = document.querySelectorAll('[content-id]');
-	const language = navigator.language;
+	const language = navigator.language || 'en-US';
 	const content = {
 		'pt-BR': {
 			'name'			: 'Renan Bastos',
-			'github'		: '&nbsp;@renanbastos93',
-			'twitter'		: '&nbsp;@renanbastos93',
-			'facebook'		: '&nbsp;/renan.bastos.925',
+			'github'		: '&nbsp;renanbastos93',
+			'twitter'		: '&nbsp;renanbastos93',
+			'facebook'		: '&nbsp;renan.bastos.925',
 			'reserved'		: 'Todos direitos reservados',
 			'bibliography' 	: 'Sou apaixonado por tecnologias, inovação e empreendedorismo. Busco sempre estar aprendendo cada dia mais, buscando coisas novas e inovando.'
 
 		},
 		'en-US': {
 			'name'			: 'Renan Bastos',
-			'github'		: '&nbsp;@renanbastos93',
-			'twitter'		: '&nbsp;@renanbastos93',
-			'facebook'		: '&nbsp;/renan.bastos.925',
+			'github'		: '&nbsp;renanbastos93',
+			'twitter'		: '&nbsp;renanbastos93',
+			'facebook'		: '&nbsp;renan.bastos.925',
 			'reserved'		: 'All right reserved',
 			'bibliography' 	: 'I am passionate about technologies, innovation and entrepreneurship. I always seek to be learning every day more, searching for new things and innovating'
 		}
