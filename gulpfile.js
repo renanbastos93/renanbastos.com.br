4ae6eda840d2768ed7044b6be0b5eb032d013605
renanbastos93 <renanbastos.tec@gmail.com>
2018-12-12 03:51:19 +0000

    feat: new struct to screen

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,31 @@
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
- 
-gulp.task('default', () =>
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const watch = require('gulp-watch');
+
+const paths = {
+	sass: 'static/assets/stylesheet/components/*.scss'
+};
+
+gulp.task('images', function(){
     gulp.src('images/*')
         .pipe(imagemin())
         .pipe(gulp.dest('img'))
-);
+});
+
+
+gulp.task('sass', function() {
+	return gulp.src('static/assets/stylesheet/components/major.scss')
+		.pipe(sass({
+			paths: ['static/assets/stylesheet/components'],
+			compress: true,
+		}))
+		.pipe(gulp.dest('static/assets/stylesheet'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch(paths.sass, ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch', 'images']);
